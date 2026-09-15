4ec8f16101c284e84ffdad34730a65daa44b855f
Matheus Tahan Sab <sabmatheus24@gmail.com>
2026-09-14 21:28:27 -0300

    Criando a base da tela

--- src/main/java/com/example/Login/application/SecureLoginPUCApplication.java
+++ src/main/java/com/example/Login/application/SecureLoginPUCApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.example"})
-public class SecureLoginApplication {
+public class SecureLoginPUCApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecureLoginApplication.class, args);
+		SpringApplication.run(SecureLoginPUCApplication.class, args);
 	}
 
 }
